3b56925d65ea4ebe05242a5f0302e8b3513d6cfb
Rikuo Hirasawa <rikuo@Nithiiyans-Air.localdomain>
2024-02-12 17:04:56 -0500

    adding error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Icon from "@iconify/svelte";
+    import { page } from "$app/stores";
+    import BombIcon from "../assets/icons/bomb.json";
+</script>
+
+<div>
+    <h1>{$page.status} </h1>
+    <Icon icon={BombIcon} width="100" />
+    <h2>{$page.error?.message}</h2>
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -6,11 +6,14 @@ export const ssr = true;
 
 export const load: Load = async ({ params }) => {
     const { slug } = params;
+    if (slug && slug.length !== 36) {
+        throw error(404, { message: "Not found" });
+    };
     if (slug) {
         const rows = await db.selectFrom("urls.markdown").selectAll().where("uuid", "=", slug).execute();
         if (!rows) {
-            throw error(404);
-        }
+            throw error(404, { message: "Not found" });
+        };
         const reactions = await db.selectFrom("urls.reactions").selectAll().where("markdown_id", "=", slug).execute();
         const data = {...rows[0], reactions: reactions.sort((a, b) => b.count - a.count)};
         return data;
